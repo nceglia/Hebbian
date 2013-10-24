34b93c98ac1f5cea19f4ee4f85919d6616d819c4
nceglia <nickceglia@gmail.com>
2013-10-23 18:15:19 -0700

    changes

--- scripts/create_job.py
+++ scripts/create_job.py
@@ -1,17 +1,18 @@
 import sys
 import os
-from subprocess import call
 import math 
+from HebbianNetwork import run
 
 def main():
-    for rate in range(1,11):
-    	for sigmoid in range(1,4):
-    		for hidden in range(10):
-    			for variables in range(5):
-    				print "Rate:",str(float(rate)/10.0),\
-    				"Sigmoid:",sigmoid,"Hidden:",hidden,"Variables:",variables,"Examples:",str(int(math.pow(2,variables))*50)
-    				call(['python','../src/python/HebbianNetwork.py','--rate',str(float(rate)/10.0),\
-    					'--sigmoid',str(sigmoid),'--hidden',str(hidden),'--variables',str(variables),\
-    					'--examples',str(int(math.pow(2,variables))*50)])
+	csv_hebbian = open("hebb_results.csv","w")
+	for variables in range(2,5):
+		for rate in range(1,5):
+			for sigmoid in range(1,4):
+				for hidden in range(6):
+					csv_hebbian.write(str(float(rate)/10.0) + '\t' + str(sigmoid) + '\t' + str(hidden) + '\t' + str(variables) + '\t' + str(int(math.pow(2,variables))*25) + '\t')
+					learned, monotone, not_learned, not_stable = run(float(rate)/10.0,sigmoid,hidden,variables,int(math.pow(2,variables))*25)
+					csv_hebbian.write(str(learned) + '\t' + str(monotone) + '\t' + not_learned + '\t' + not_stable + '\n')
+	csv_hebbian.close()
+
 if __name__=="__main__":
-    main()
+	main()
